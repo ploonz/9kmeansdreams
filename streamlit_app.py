7721b93caadcc25427779af8904b54341d19e221
ploonz <voropaevsg@gmail.com>
2025-05-18 15:23:04 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,8 +50,8 @@ with col1:
     st.subheader("📋 Информация о клиенте")
     client_data=df[df['customer_id']==client_id]
     avg_price=client_data['price'].mean()
-
-    st.metric(f"Штат",f"{BRAZIL_STATES.get(client_data['state'].to_string(index=False).split()[0], client_data['state'].to_string(index=False).split()[0])}")
+    st=BRAZIL_STATES.get(client_data['state'].to_string(index=False).split()[0]
+    st.metric(f"Штат",f"{st, st)
     st.metric(f"Средняя стоимость заказа",f"R${avg_price}")
 with col2:
     risk = df[df["customer_id"] == client_id]["cluster"].values[0]
